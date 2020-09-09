01b56a5b7b279a36b3d745f00638c49e2cc1e771
Mhelp <HbcPxE468>
2020-09-09 22:27:15 +0100

    1 er commit -manque la pagination -manque partie admins -mettre image par défaut sur le profil -manque la partie contact -manque le champ prix -les catégories sont écrites en dur

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,14 @@
+                   </div>
+               </div>
+           </div>
+        </div>
+     </div>
+  </div> <!-- /container -->
+  <footer>
+      <p class="text-center">Marochelp.com ©2020</p>
+  </footer>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  </body>
+</html>
---
+++ codeigniter-annonces.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 09 sep. 2020 à 21:23
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `codeigniter-annonces`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ads`
+--
+
+DROP TABLE IF EXISTS `ads`;
+CREATE TABLE IF NOT EXISTS `ads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `ads`
+--
+
+INSERT INTO `ads` (`id`, `user_id`, `category`, `title`, `body`, `city`, `image`, `created`) VALUES
+(14, 5, 'Informatique & Téchonologie', 's de sites Web ont fait du Lorem Ipsum leur faux texte pa', 'Plusieurs variations de Lorem Ipsum peuvent être trouvées ici ou là, mais la majeure partie d\'entre elles a été altérée par l\'addition d\'humour ou de mots aléatoires qui ne ressemblent pas une seconde à du texte standard. Si vous voulez utiliser un passag', 'Rabat', 'residence-2219972_1280.jpg', '2020-09-09 21:29:09'),
+(12, 3, 'Animaux', 'chien caniche', 'chien de bonne race avec un bon prix', 'Rabat', 'animal-1284307_640.jpg', '2020-09-09 21:26:09'),
+(13, 5, 'Animaux', 'chien loup', 'chien loup fais plusieur \r\nLe Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla e', 'Tanger', 'pug-690566_1280.jpg', '2020-09-09 21:44:09'),
+(10, 3, 'Informatique & Téchonologie', 'cherche développeur', 'développeur doit tout faire maitriser tous les langage de programmation', 'Casablanca', 'technology-785742_640.jpg', '2020-09-09 21:00:09'),
+(11, 3, 'Informatique & Téchonologie', 'developpeur java fullstack', 'développement jquery\r\njavascript\r\nphp', 'Casablanca', 'office-620822_640.jpg', '2020-09-09 21:50:09');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` int(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `Tel` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `city`, `Tel`) VALUES
+(4, 'ali', 'mrabete', 'mrabet@gmail.com', 6, 'casablanca', 663215200),
+(3, 'salim', 'toto', 'toto@gmail.com', 6, 'casablanca', 663215252),
+(5, 'sss', 'massimo', 'massimo@gmail.com', 6, 'casablanca', 663217744);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
